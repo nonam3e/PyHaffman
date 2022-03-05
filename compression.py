29515b4560a566f1f2734d9c6d5372847588a214
noname <lendedland@gmail.com>
2022-03-05 22:18:43 +0700

    weird bubylda compression added

--- compression.py
+++ compression.py
@@ -22,15 +22,15 @@ def compress():
         else:
             counter[chr(letter)] += 1
     counter = sorted(counter.items(), key=lambda x: x[1])
-    #print(counter)
+    # print(counter)
 
     nodes = []
     for item in counter:
         nodes.append(BinaryNode(content=item[0], prob=item[1]))
 
     while len(nodes) > 1:
-        right = nodes[0]
-        left = nodes[1]
+        left = nodes[0]
+        right = nodes[1]
         right.code = 0
         left.code = 1
         new_node = BinaryNode(prob=left.prob + right.prob, content=left.content + right.content, left=left, right=right)
@@ -38,9 +38,31 @@ def compress():
         nodes.remove(left)
         nodes.append(new_node)
         nodes = sorted(nodes, key=lambda x: x.prob)
-    print(nodes[0].get_dict())
+        # for node in nodes:
+        #     print(f"{node.content}:{node.prob}",end=' ')
+        # print("\n----")
 
+    codes = nodes[0].get_dict()
+    print(codes)
+    print(counter)
+    raw_size = len(content) * 8
+    compressed_size = 0
+    for item in counter:
+        compressed_size += item[1] * len(codes[item[0]])
+    print(f"raw: {raw_size} compressed: {compressed_size}")
+    empty_bits = (8 - compressed_size % 8) % 8
+    print(empty_bits)
     utils.print_hashsum(content)
+    output = open(f"{pathlib.Path(name).stem}.bubylda", "wb")
+    buffer = "0" * empty_bits
+    for word in content:
+        buffer += codes[chr(word)]
+        # while len(buffer) >= 8:
+        #     output.write((int(buffer[:8],2)))
+        #     buffer = buffer[8:]
+    print(buffer)
+    print(len(buffer))
+    output.write(int(buffer, 2).to_bytes(len(buffer) // 8, byteorder='big'))
 
 
 if __name__ == '__main__':
